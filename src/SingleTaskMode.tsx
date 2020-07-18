477adffee5e7cc9715fd98cb430fa2438ccc3b4e
Marta WIld <levasheva95@mail.ru>
2020-07-18 18:43:30 +0500

    Mockup finished

--- src/SingleTaskMode.tsx
+++ src/SingleTaskMode.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    margin-left: 20px;
+`;
+
+const WrapperTodo = styled.div`
+    display: flex;
+    flex-direction: row; 
+`;
+
+const TodoText = styled.li`
+  list-style-type: none;
+  margin-right: 10px;
+`;
+
+const Time = styled.div``;
+
+const WrapperButtons = styled.div``;
+
+const DeleteButton = styled.button``;
+
+const CompleteButton = styled.button``;
+
+const NextButton = styled.button``;
+
+const WrapperTimer = styled.div`
+    display: flex;
+    flex-direction: row; 
+`;
+
+const Timer = styled.div``;
+
+const StartButton = styled.button``;
 
 export default function SingleTaskMode() {
     return (
         <Wrapper>
-            fghfghfh
+            <WrapperTodo>
+                <TodoText>sdfsdf</TodoText>
+                <Time>15:00</Time>
+            </WrapperTodo>
+            <WrapperButtons>
+                <DeleteButton>X</DeleteButton>
+                <CompleteButton>Check</CompleteButton>
+                <NextButton>→</NextButton>
+            </WrapperButtons>
+            <WrapperTimer>
+                <Timer> 0:00 / 1:15 </Timer>
+                <StartButton>►</StartButton>
+            </WrapperTimer>
         </Wrapper>
     )
 }
